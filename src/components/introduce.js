b7744cab7f00503634b695939e1f4ca49f2e57f0
ryanhtanto <ryanherrtanto@gmail.com>
2023-05-23 13:36:02 +0700

    update introduce.js

--- src/components/introduce.js
+++ src/components/introduce.js
@@ -27,11 +27,11 @@ const Introduce = () => {
                                         <p className="mt-5">Based in the metropolitan city of Tangerang, Indonesia. I am currently exploring as a programmer in <span className="fw-bold" style={{color: '#D3902A'}}>frontend web developer,</span> and got course certification from <a href="https://www.dicoding.com/certificates/RVZKK9GOOZD5" style={{textDecoration:'none', fontWeight: '700'}}><span className="fw-bold dicoding-certification">DICODING academy.</span></a></p>
                                         <p className="mt-3">I have experienced in frontend developer for one years, and exploring some programming language such as Javascript, PHP, Java, and Typescript, with their framework.</p>
                                 </div>
-                                <div className="col-lg-5 col-md-12 col-sm-12 d-flex align-items-center justify-content-center">
+                                {/* <div className="col-lg-5 col-md-12 col-sm-12 d-flex align-items-center justify-content-center">
                                         <div className="card">
                                                 <img src={Avatar} alt="avatar" className="card-img-top"/>
                                         </div>
-                                </div>
+                                </div> */}
                         </div>
                 </div>
         </>
